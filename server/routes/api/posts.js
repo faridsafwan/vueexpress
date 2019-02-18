df715ca6fbc7e32a77efee21d5813c059a09a327
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-02-18 10:46:47 +0800

    add catch

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -27,16 +27,21 @@ router.delete('/:id', async (req, res) => {
 });
 
 async function loadPostsCollection() {
-  const client = await mongodb.MongoClient.connect(
-    'mongodb+srv://admin:admin@firstcluster-lic1d.mongodb.net/test?retryWrites=true',
-    // 'mongodb://admin:admin@firstcluster-shard-00-00-lic1d.mongodb.net:27017,firstcluster-shard-00-01-lic1d.mongodb.net:27017,firstcluster-shard-00-02-lic1d.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
-    {
-      useNewUrlParser: true
-    }  ).catch(err => {
-      console.log(err)
-    });
-
-  return client.db('vue_express').collection('posts');
+  try {
+    const client = await mongodb.MongoClient.connect(
+      'mongodb+srv://admin:admin@firstcluster-lic1d.mongodb.net/test?retryWrites=true',
+      // 'mongodb://admin:admin@firstcluster-shard-00-00-lic1d.mongodb.net:27017,firstcluster-shard-00-01-lic1d.mongodb.net:27017,firstcluster-shard-00-02-lic1d.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
+      {
+        useNewUrlParser: true
+      }  ).catch(err => {
+        console.log(err)
+      });
+  
+    return client.db('vue_express').collection('posts');  
+  } catch (error) {
+    console.log(error)
+  }
+ 
 }
 
 module.exports = router;
