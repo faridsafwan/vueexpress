6c9f389c4a21051efdb8310d5211941ad9fa5e06
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-02-18 15:04:22 +0800

    add urlencoded

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 //Middleware 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
