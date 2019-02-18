0b1cd3ac6e90e54250fa39f8817a46a3ddef1130
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-02-18 15:07:29 +0800

    update

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 // Get Posts
 router.get('/', async (req, res) => {
-  try {
+  // try {
     const posts = await loadPostsCollection();  
     res.send(await posts.find().toArray());
-  } catch (error) {
-    return res.send(error)
-  }
+  // } catch (error) {
+  //   return res.send(error)
+  // }
 
 });
 
@@ -32,7 +32,7 @@ router.delete('/:id', async (req, res) => {
 });
 
 async function loadPostsCollection() {
-  try {
+  // try {
     const client = await mongodb.MongoClient.connect(
       'mongodb+srv://admin:admin@firstcluster-lic1d.mongodb.net/test?retryWrites=true',
       // 'mongodb://admin:admin@firstcluster-shard-00-00-lic1d.mongodb.net:27017,firstcluster-shard-00-01-lic1d.mongodb.net:27017,firstcluster-shard-00-02-lic1d.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
@@ -43,9 +43,9 @@ async function loadPostsCollection() {
       });
   
     return client.db('vue_express').collection('posts');  
-  } catch (error) {
-    console.log(error)
-  }
+  // } catch (error) {
+  //   console.log(error)
+  // }
  
 }
 
