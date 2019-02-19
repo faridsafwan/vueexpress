6a01e6f5f3fddbead5e754312d9b7adbe55a581d
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-02-19 14:21:58 +0800

    update

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 //Middleware 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
