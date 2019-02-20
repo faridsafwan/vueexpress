c9ed436b9df1b2141f7b310634716c1dea536a6e
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-02-20 13:56:09 +0800

    update

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-//Middleware 
+// Middleware
+app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json());
 
 const posts = require('./routes/api/posts');
 
-app.use('/api/posts',posts);
+app.use('/api/posts', posts);
 
 // Handle production
-if(process.env.NODE_ENV === 'production'){
-    // Static folder
-    app.use(express.static(__dirname + '/public/'))
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
 
-    // Handle SPA
-    app.get(/.*/, (req,res) => res.sendFile(__dirname + '/public/index.html'))
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
