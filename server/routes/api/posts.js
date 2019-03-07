2e3e1cc32dc09b3d33a0339c7138c37f21cf9a85
Muhamad Farid Safwan <faridsafwan@Muhamads-MacBook-Pro.local>
2019-03-07 11:14:58 +0800

    latest

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,24 +1,20 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
 const router = express.Router();
 
 // Get Posts
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const posts = await loadPostsCollection();  
-    console.log(posts);
-    
+    const posts = await loadPostsCollection();
     res.send(await posts.find({}).toArray());
   } catch (error) {
-    console.log(error);
-    
-    return res.send(error)
+    return res.send(error);
   }
 });
 
 // Add Post
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const posts = await loadPostsCollection();
   await posts.insertOne({
     text: req.body.text,
@@ -28,7 +24,7 @@ router.post('/', async (req, res) => {
 });
 
 // Delete Post
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   const posts = await loadPostsCollection();
   await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
   res.status(200).send();
@@ -36,18 +32,18 @@ router.delete('/:id', async (req, res) => {
 
 async function loadPostsCollection() {
   // try {
-    const client = await mongodb.MongoClient.connect(
-      `mongodb+srv://admin:admin@firstcluster-lic1d.mongodb.net/test?retryWrites=true`,
-      // 'mongodb://admin:admin@firstcluster-shard-00-00-lic1d.mongodb.net:27017,firstcluster-shard-00-01-lic1d.mongodb.net:27017,firstcluster-shard-00-02-lic1d.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
-      {
-        useNewUrlParser: true
-      }  );
-  
-    return client.db('vue_express').collection('posts');  
+  const client = await mongodb.MongoClient.connect(
+    `mongodb+srv://admin:admin@firstcluster-lic1d.mongodb.net/test?retryWrites=true`,
+    // 'mongodb://admin:admin@firstcluster-shard-00-00-lic1d.mongodb.net:27017,firstcluster-shard-00-01-lic1d.mongodb.net:27017,firstcluster-shard-00-02-lic1d.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
+    {
+      useNewUrlParser: true
+    }
+  );
+
+  return client.db("vue_express").collection("posts");
   // } catch (error) {
   //   console.log(error)
   // }
- 
 }
 
 module.exports = router;
